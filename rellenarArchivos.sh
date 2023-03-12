1a30ed887dcc8b9bba0bdd4cfb2e05a053d607ea
Oscar992 <oscarmanuel.2810@gmail.com>
2023-03-12 14:14:03 -0500

    subir demas archivos

---
+++ rellenarArchivos.sh
@@ -0,0 +1,35 @@
+cd Root/FileA/FileA1
+
+echo 'Hola, soy el archivo 1
+Este es mi contenido, repito tengo el valor 1
+Hasta Luego' >> archivo1.txt
+
+echo 'Hola, soy el archivo 2
+Este es mi contenido, repito tengo el valor 2
+Hasta Luego' >> archivo2.txt
+
+cd ..
+cd FileA2
+
+echo 'Hola, soy el archivo 3
+Este es mi contenido, repito tengo el valor 3
+Hasta Luego' >> .archivo3.txt
+
+cd ..
+cd ..
+cd FileB/FileB1
+echo 'Hola, soy el archivo 4
+Este es mi contenido, repito tengo el valor 4
+Hasta Luego' >> .archivo4.txt
+
+echo 'Hola, soy el archivo 5
+Este es mi contenido, repito tengo el valor 5
+Hasta Luego' >> archivo5.txt
+
+cd ..
+cd ..
+
+cd FileC
+echo 'Hola, soy el archivo 6
+Este es mi contenido, repito tengo el valor 6
+Hasta Luego' >> archivo6.txt
